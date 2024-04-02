d7e12f38d85c07d4cb0daaf4acb7f5a0efc91638
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-04-01 23:03:44 -0400

    refactor main menu

---
+++ src/app/components/MainMenu.tsx
@@ -0,0 +1,22 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem
+} from '@nextui-org/react'
+import { LoginButton } from './LoginButton'
+
+export function MainMenu() {
+  return (
+    <Navbar shouldHideOnScroll>
+      <NavbarBrand>
+        <p className='font-bold text-inherit'>SimpleLoad</p>
+      </NavbarBrand>
+      <NavbarContent justify='end'>
+        <NavbarItem>
+          <LoginButton />
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
+}
