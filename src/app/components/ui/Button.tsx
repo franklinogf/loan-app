4c9c9ba401fda855286ce9a7d641f090821f4e4e
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-04-01 22:32:51 -0400

    removed custom button

--- src/app/components/ui/Button.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import { extendVariants, Button as NextUIButton } from '@nextui-org/react'
-
-export const Button = extendVariants(NextUIButton, {
-  defaultVariants: { color: 'primary' }
-})
