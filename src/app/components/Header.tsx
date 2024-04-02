8a1d58dbe5aee00d4c05215584dfbe8f7ee60713
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-04-01 23:04:02 -0400

    refactor and updated classes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,58 +1,28 @@
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Link,
-  Button,
-  Chip
-} from '@nextui-org/react'
+import { Chip } from '@nextui-org/react'
+import { MainMenu } from './MainMenu'
 
 export default function Header() {
   return (
     <header>
-      <div>
-        <article className='mb-20'>
-          <Navbar shouldHideOnScroll>
-            <NavbarBrand>
-              <p className='font-bold text-inherit'>SimpleLoad</p>
-            </NavbarBrand>
-            <NavbarContent justify='end'>
-              <NavbarItem>
-                <Link href='#'>Login</Link>
-              </NavbarItem>
-              <NavbarItem>
-                <Button
-                  as={Link}
-                  color='primary'
-                  href='#'
-                  variant='flat'
-                >
-                  Sign Up
-                </Button>
-              </NavbarItem>
-            </NavbarContent>
-          </Navbar>
-        </article>
-        <article className='flex flex-col items-center mx-auto text-center gap-y-10'>
-          <Chip color='primary'>TRUSTED BY OVER +55,000</Chip>
-          <h1 className='font-bold text-6xl w-[700px]'>
-            The guide to launching your successful startup.
-          </h1>
-          <p className='text-xl w-[700px]'>
-            Leverage the potential of cutting-edge AI-driven algorithms, dynamic
-            data analytics, and efficient workflow streamlining to pinpoint
-            strategic opportunities for expansion, expenditure minimization, and
-            opera.
-          </p>
-          <Chip
-            color='default'
-            className='text-md'
-          >
-            No Credit Card Required . Cancel Anytime
-          </Chip>
-        </article>
-      </div>
+      <MainMenu />
+      <section className='flex flex-col items-center gap-y-10 mt-20'>
+        <Chip color='primary'>TRUSTED BY OVER +55,000</Chip>
+        <h1 className='font-bold text-5xl max-w-xl text-center text-balance'>
+          The guide to launching your successful startup.
+        </h1>
+        <p className='text-lg max-w-xl text-pretty text-center opacity-80'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo assumenda
+          perferendis est modi, aliquid distinctio, omnis accusamus autem,
+          placeat perspiciatis rem reiciendis minima minus! Qui explicabo fugit
+          placeat necessitatibus molestiae.
+        </p>
+        <Chip
+          color='default'
+          className='text-md'
+        >
+          No Credit Card Required . Cancel Anytime
+        </Chip>
+      </section>
     </header>
   )
 }
