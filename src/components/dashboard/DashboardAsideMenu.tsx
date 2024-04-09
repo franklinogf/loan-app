9d3274454381ed82cf6431234ca14a70431e1ce7
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-04-08 21:35:32 -0400

    aside menu when lg with menu items

--- src/components/dashboard/DashboardAsideMenu.tsx
+++ src/components/dashboard/DashboardAsideMenu.tsx
@@ -1,47 +1,44 @@
-import { List, ListItem, UnorderedList } from '@chakra-ui/react'
+'use client'
+import { Button, List, ListItem } from '@chakra-ui/react'
 import {
   IconBuildingStore,
   IconDashboard,
-  IconDirectionSign,
-  IconFingerprint,
   IconInbox,
   IconTriangleSquareCircle,
   IconUsers
 } from '@tabler/icons-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
+const menuItems = [
+  { label: 'Dashboard', href: '/dashboard', icon: <IconDashboard /> },
+  { label: 'Kanban', href: '/kanban', icon: <IconTriangleSquareCircle /> },
+  { label: 'Mensajes', href: '/inbox', icon: <IconInbox /> },
+  { label: 'Usuarios', href: '/users', icon: <IconUsers /> },
+  { label: 'Productos', href: '/products', icon: <IconBuildingStore /> }
+]
 export function DashboardAsideMenu() {
+  const pathname = usePathname()
   return (
-    <List>
-      <UnorderedList>
-        <ListItem className='flex'>
-          <IconDashboard />
-          Dashboard
+    <List
+      spacing={3}
+      px={5}
+    >
+      {menuItems.map(({ label, href, icon }, index) => (
+        <ListItem key={index}>
+          <Button
+            as={Link}
+            href={href}
+            w='100%'
+            size='md'
+            leftIcon={icon}
+            variant={pathname !== href ? 'ghost' : 'solid'}
+            justifyContent='flex-start'
+          >
+            {label}
+          </Button>
         </ListItem>
-        <ListItem className='flex'>
-          <IconTriangleSquareCircle />
-          Kanban
-        </ListItem>
-        <ListItem className='flex'>
-          <IconInbox />
-          Mensajes
-        </ListItem>
-        <ListItem className='flex'>
-          <IconUsers />
-          Usuarios
-        </ListItem>
-        <ListItem className='flex'>
-          <IconBuildingStore />
-          Productos
-        </ListItem>
-        <ListItem className='flex'>
-          <IconDirectionSign />
-          Iniciar Sesión
-        </ListItem>
-        <ListItem className='flex'>
-          <IconFingerprint />
-          Registrarse
-        </ListItem>
-      </UnorderedList>
+      ))}
     </List>
   )
 }
