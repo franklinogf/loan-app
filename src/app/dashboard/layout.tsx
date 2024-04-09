25faed11b2b400a12a350e878e1cf3c0330737cf
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-04-08 21:35:14 -0400

    layout aside to be hidden on lg

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,12 +9,12 @@ export default function RootLayout({
     <>
       <DashboardMenu />
       <aside
-        className='fixed top-0 left-0 w-64 h-screen pt-20 transition-transform -translate-x-full  border-r border-gray-200 md:translate-x-0 '
+        className='fixed top-0 left-0 w-64 h-screen pt-20 transition-transform -translate-x-full  border-r border-gray-200 lg:translate-x-0 '
         aria-label='Sidebar'
       >
         <DashboardAsideMenu />
       </aside>
-      <main className='p-4 md:ml-64'>{children}</main>
+      <main className='p-4 lg:ml-64'>{children}</main>
     </>
   )
 }
