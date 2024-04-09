4535e1f7f3fbb5e819acedfa5e57e1daa141bf1a
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-04-08 21:34:37 -0400

    customized breakpoint to be the same as tailwindcss

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,6 +2,14 @@ import { extendTheme } from '@chakra-ui/react'
 import colors from '@/theme/colors'
 import components from '@/theme/components'
 const theme = extendTheme({
+  breakpoints: {
+    'base': '0px',
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+    '2xl': '1536px'
+  },
   colors,
   components
 })
